714d77a7c57ef56fcb49e90d425ef679db72684d
Jerry Guerra <jerry.gdr13@gmail.com>
2021-08-09 20:00:17 -0600

    commit

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -7,8 +7,22 @@ def main():
     x, y = 10, 100
 
     # conditional flow uses if, elif, else
+    if (x < y):
+        st = " X is less than y"
+    elif (x == y):
+        st = "x is the same as y"
+    else:
+        st = "X is greater than y"
+
+    
+    print(st)
+
 
     # conditional statements let you use "a if C else b"
+    st = "X is greater than y" if(x < y) else "X is less or equal than y"
+    
+    print(st)
+
 
 
 if __name__ == "__main__":
