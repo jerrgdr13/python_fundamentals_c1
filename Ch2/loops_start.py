d19c81263a0277af216ccd6e4fceb3c9ab5dac14
Jerry Guerra <jerry.gdr13@gmail.com>
2021-08-10 18:16:52 -0600

    commit

--- Ch2/loops_start.py
+++ Ch2/loops_start.py
@@ -6,18 +6,37 @@ def main():
   x = 0
 
   # define a while loop
+  #while (x<5):
+  #  print(x)
+  #  x = x +1
 
 
   # define a for loop
+ # for x in range(1,10):
+ #   print(x)
 
 
-  # use a for loop over a collection
+  ## use a for loop over a collection
+  #days = ["Mon", "Tus", "Wed", "Thu", "Fri", "Sat", "Sun"]
+#
+  #for d in days:
+  #  print(d)
 
   
   # use the break and continue statements
+  #for x in range(1,10):
+  #  #if(x == 7): break
+  #  if(x % 2 == 0): continue
+  #  print(x)
 
 
   #using the enumerate() function to get index 
+  days = ["Mon", "Tus", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+  for d,i in enumerate(days):
+    print(d,i)
+
+
   
 if __name__ == "__main__":
   main()
